5242d448305f04a254a6fd079047eff8eeca5111
Avishai <avishai@Avishais-Mac.local>
2022-01-23 15:30:07 +0200

    first commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "region" {
+  default = "eu-central-1" //Frankfurt region
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+    "331173606331"
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::331173606331:root"
+      username = "root"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::331173606331:user/Avishai-proj"
+      username = "Avishai"
+      groups   = ["system:masters"]
+    }
+  ]
+}
